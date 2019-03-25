0781914b84f266f8481312866cdf4ae80c4751d9
Catherine Blatter <catherine.blatter@unibas.ch>
2019-03-25 19:46:15 +0100

    first push

---
+++ 00_publish_public.R
@@ -0,0 +1,48 @@
+###############################################
+## General instructions for publishing setup ##
+###############################################
+
+
+# 1. Prefix ---------------------------------------------------------------
+## install necessary packages 
+
+# devtools to install from github rather than CRAN
+if (!require('devtools')) {
+  install.packages("devtools")
+}
+
+# Rwordpress and XLMRPC
+if (!require('RWordPress')) {
+  devtools::install_github(c("duncantl/XMLRPC", "duncantl/RWordPress"))
+}
+
+
+# load required packages
+
+library(XMLRPC)
+library(RWordPress)
+library(knitr)
+
+# 2. Setup your user account -------------------------------------------
+
+# set you personal options --> change username and 'password'
+options(WordpressLogin = c(username = 'password'),
+        WordpressURL = 'https://psq.nursing.unibas.ch/xmlrpc.php')
+
+
+# this command lets you then update
+id <- knit2wp('yourfile.Rmd', 
+        title = "Your posts title",
+        categories = "Code",
+        action = "newPost",
+        publish = FALSE)
+
+
+knit2wp('yourfile.Rmd', 
+        postid = id,
+        title = 'Your post title',
+        shortcode = F, 
+        action = 'editPost', 
+        publish = F)
+
+
